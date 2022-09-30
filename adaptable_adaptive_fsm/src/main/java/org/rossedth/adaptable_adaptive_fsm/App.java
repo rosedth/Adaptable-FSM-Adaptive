ad4e56221e06866eb173c3a61f9425db1b3b4c50
Rosalia Caya <rossedth@gmail.com>
2022-09-30 18:29:23 -0300

    Uploading project

---
+++ adaptable_adaptive_fsm/src/main/java/org/rossedth/adaptable_adaptive_fsm/App.java
@@ -0,0 +1,13 @@
+package org.rossedth.adaptable_adaptive_fsm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
